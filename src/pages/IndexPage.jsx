e3316eaf489ae2d8e803fc333d2caf240d664628
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-07-02 19:28:52 +0530

    create layout and add login route with login page

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,14 @@
+const IndexPage = () => {
+  return (
+    <div>
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">Welcome to Airbnb</h1>
+        <p className="text-gray-500">
+          Find the perfect rental for your next vacation.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default IndexPage;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,37 @@
+import { Field, Input } from "@headlessui/react";
+import { Link } from "react-router-dom";
+
+const LoginPage = () => {
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto">
+          <Field>
+            <Input
+              className="w-full border my-1 py-2 px-3 rounded-2xl"
+              type="email"
+              placeholder="your@email.com"
+            />
+
+            <Input
+              className="w-full border my-1 py-2 px-3 rounded-2xl"
+              type="password"
+              placeholder="password"
+            />
+            <button className="primary">Login</button>
+
+            <div className="py-2 text-center text-gray-500">
+              Don&apos;t have an account?{" "}
+              <Link className="underline text-primary" to={"/register"}>
+                Register now
+              </Link>
+            </div>
+          </Field>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
